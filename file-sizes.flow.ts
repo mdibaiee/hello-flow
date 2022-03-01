cb4dea545604dd6aa4f41ab7a477ae4a4db18d4b
Mahdi Dibaiee <mahdi@Mahdis-MacBook-Pro.local>
2022-03-01 17:25:14 +0000

    file-sizes flow

---
+++ file-sizes.flow.ts
@@ -0,0 +1,12 @@
+import { collections, interfaces, registers /*, anchors*/ } from 'flow/modules';
+
+// Implementation for derivation word-counts.flow.yaml#/collections/acmeCo~1word-counts/derivation.
+export class AcmeCoFileSizes implements interfaces.AcmeCoFileSizes {
+    fromDocumentsPublish(
+        source: collections.AcmeCoFiles,
+        _register: registers.AcmeCoFileSizes,
+        _previous: registers.AcmeCoFileSizes,
+    ): collections.AcmeCoFileSizes[] {
+        return [{ file: source._meta.file, size: source.body.length }];
+    }
+}
